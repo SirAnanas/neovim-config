f7fcc5ffc89d87fc2b28d851f9914a9de0e653c7
Ananas <insecurestone@gmail.com>
2024-09-05 22:12:32 +0200

    fixed script

--- neovim-install.sh
+++ neovim-install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt-get update
-apt-get -y install git gcc curl unzip python3-dev python3-pip python3.8-venv python-dev software-properties-common
-add-apt-repository -y ppa:neovim-ppa/unstable
-apt-get update
-apt-get -y install neovim
-mkdir -p /home/zsrckp/.config/nvim
-cd /home/zsrckp/.config/nvim
+sudo apt-get update
+sudo apt-get -y install git gcc curl unzip python3-dev python3-pip python3.8-venv python-dev software-properties-common
+sudoadd-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get -y install neovim
+mkdir -p ~/.config/nvim
+cd ~/.config/nvim
 git clone https://github.com/SirAnanas/neovim-config/
 mv neovim-config/* .
 rm -rf neovim-config
