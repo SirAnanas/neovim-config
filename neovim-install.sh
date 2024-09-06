2b1033ced3142dd71f9b65f7ef29d0a9c15ae855
Ananas <insecurestone@gmail.com>
2024-09-06 20:01:13 +0200

    fixed script

--- neovim-install.sh
+++ neovim-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install git gcc curl unzip python3-dev python3-pip python3.8-venv python-dev software-properties-common
-sudoadd-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt-get -y install git gcc curl unzip python3.8-venv software-properties-common
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get -y install neovim
 mkdir -p ~/.config/nvim
