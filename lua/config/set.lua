b10923019fbb077229e0401305ddf6a66427fc00
Ananas <insecurestone@gmail.com>
2024-09-06 20:18:51 +0200

    script

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,9 +1,11 @@
 vim.g.mapleader = " "
 
-vim.o.tabstop = 4 
-vim.o.expandtab = true 
-vim.o.softtabstop = 4 
-vim.o.shiftwidth = 4 
+vim.o.termguicolors = true
+
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
 vim.o.nu = true
 vim.o.relativenumber = true
--- neovim-install.sh
+++ neovim-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install git gcc curl unzip python3.8-venv software-properties-common
+sudo apt-get -y install git gcc curl python3-dev python3-pip python-dev ripgrep unzip python3.8-venv software-properties-common
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get -y install neovim
