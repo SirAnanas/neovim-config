2628b09d7229cf1947e6449f178aaa7cdbcb758e
Ananas <insecurestone@gmail.com>
2024-09-06 20:25:52 +0200

    f

--- neovim-install.sh
+++ neovim-install.sh
@@ -6,7 +6,4 @@ sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get -y install neovim
 mkdir -p ~/.config/nvim
-cd ~/.config/nvim
-git clone https://github.com/SirAnanas/neovim-config/
-mv neovim-config/* .
-rm -rf neovim-config
+mv * ~/.config/nvim
